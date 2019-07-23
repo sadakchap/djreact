7173e86273da541ec3c0c3b73572ff21a71cac14
prerna <aliceprerna@gmail.com>
2019-07-23 10:22:28 +0530

    complete CRUD

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,8 +1,25 @@
-from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from articles.api.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', ArticleViewSet, basename='article')
+urlpatterns = router.urls
 
 
-urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('<int:pk>/', ArticleDetailView.as_view()),
-]
+# from django.urls import path
+# from .views import (
+#     ArticleListView, 
+#     ArticleDetailView, 
+#     ArticleCreateView,
+#     ArticleUpdateView,
+#     ArticleDeleteView
+#  )
+
+
+# urlpatterns = [
+#     path('', ArticleListView.as_view()),
+#     path('create/', ArticleCreateView.as_view()),
+#     path('update/<int:pk>/', ArticleUpdateView.as_view()),
+#     path('delete/<int:pk>/', ArticleDeleteView.as_view()),
+#     path('<int:pk>/', ArticleDetailView.as_view()),
+# ]
--- backend/src/articles/models.py
+++ backend/src/articles/models.py
@@ -6,10 +6,10 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Article(models.Model):
-    author  = models.ForeignKey(User, on_delete=models.CASCADE)
-    title   = models.CharField(max_length=120)
-    content = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+    author      = models.ForeignKey(User, on_delete=models.CASCADE)
+    title       = models.CharField(max_length=120)
+    content     = models.TextField()
+    created     = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
